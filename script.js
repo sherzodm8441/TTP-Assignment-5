cee71b8df0c713b8ed53aa8b45d33b29955c5842
Sherzod Mirsharipov <mirsharipovsherzod@gmail.com>
2022-01-09 02:38:21 -0500

    closes #9; add removeRow feature

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ function addRow(){ //add a row when called
 }
 
 
+function removeRow(){ //removes a row when called
+    //grab all the existing columns
+    const colEls = document.getElementsByClassName('column')
+
+
+    for(let item of colEls){
+        //in each column...
+        const div = item.getElementsByClassName('box')
+
+        //...remove the last box div, effectively removing a row
+        if(div.length > 1){ //makes sure theres at least one row remaining
+            div[div.length - 1].remove()
+        }
+        
+    }
+}
