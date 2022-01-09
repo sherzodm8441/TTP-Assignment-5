fe931015c470cd52854bfc4d1369584b8a2b544b
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2022-01-08 20:55:02 -0500

    Properly adds new cells to the new column
    Before, new cells would be distributed through all columns.
    I foresaw that this would be a problem, since we are planning on having
    the user style the color of the cells. Therefore, it is pertinent that
    we keep the order of the cells in order to avoid user frustration when
    their cells shift by adding a new column.
    
    To remedy this, I modified the fucntion to take in the children elements
    and insert new cells by going down the children collection. The decrement
    value is taken based on the number of columns (before adding a new
    column).
    
    IMPORTANT! The children collection is a live HTML collection, which
    means that to iterate over the elements as one would do with an array,
    we must decrement from the end of the collection. Incrementing would not
    work as we are adding elements, which increases the length of the array
    and thus is likely to be problematic.
    
    See the section "Creating Nodes" in this link for a brief introduction
    to live collections: https://eloquentjavascript.net/14_dom.html

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 const gridBoardEl = document.getElementById("grid-board")
 const addColBtn = document.getElementById("addCol-btn")
-const cell = `<div class="box"></div>` // Use this variable to add a cell
-const rowTemplateValue = "100px 100px"
-const colTemplateValue = "100px 100px"
-let tempString = ""
+const removeColBtn = document.getElementById("removeCol-btn")
+const rowTemplateValue = "100px 100px 100px"
+const colTemplateValue = "100px 100px 100px"
+let tempString = "" // Will be used in several functions
+const gridChildNodes = gridBoardEl.children
+
 
 // The below code is used to add a column to a filled grid
 
 gridBoardEl.style.gridTemplateColumns = colTemplateValue
 
 const addCol = () => {
+    let cell
+    
+    //Find the number of columns so we know where to add new cells 
+    tempString = gridBoardEl.style.gridTemplateColumns.split(" ")
+    let cellsToSkip = tempString.length // number to decrement for loop by
+
 
     // Adding a new value creates a new column. Note the space.
-    gridBoardEl.style.gridTemplateColumns += " 100px"
-
-    //Find number of rows
-    tempString = rowTemplateValue.split(" ")
-
-    // Add an element for every row (to fill the grid)
-    for (let i = 0; i < tempString.length; i++) {
-        gridBoardEl.innerHTML += cell
+    gridBoardEl.style.gridTemplateColumns += " 100px"   
+    let cellElements = gridChildNodes.length
+    
+    
+    // Add an element after the last element of a row
+    // We know where this is based on the number of columns
+    for (let i = cellElements - 1; i >= 0; i -= cellsToSkip) {
+        cell = document.createElement("div")
+        cell.setAttribute("class", "box")
+        gridChildNodes[i].after(cell)
     }
 
 }
