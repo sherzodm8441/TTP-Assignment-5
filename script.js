f5de9da1886f7c0e1580013916ebd913e9b2d93c
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2022-01-08 21:25:46 -0500

    Added remove column button and functionality.
    The remove column functionality removes the right-most column along with
    the appropriate cells. The process is similar to the add column feature,
    except that we are slicing off a column declaration in the style
    property, and instead of adding new cells using a for loop, we are
    removing the current cell.
    
    Note the following edge case: What happens if there are no columns to
    remove? Answer: The for loop is not entered since i will be less than 0
    due to the HTML Collection having a length of 0, and i starts at the
    length - 1.
    
    But so does the for loop in the add function. Therefore, we cannot add
    columns if all of them are removed. To fix this, an if statement was
    added where it adds one cell if there are no cells.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const addColBtn = document.getElementById("addCol-btn")
 const removeColBtn = document.getElementById("removeCol-btn")
 const rowTemplateValue = "100px 100px 100px"
 const colTemplateValue = "100px 100px 100px"
-let tempString = "" // Will be used in several functions
+let tempString = "" // Will be used in several functions as a temp variable
 const gridChildNodes = gridBoardEl.children
 
 
@@ -22,8 +22,15 @@ const addCol = () => {
     // Adding a new value creates a new column. Note the space.
     gridBoardEl.style.gridTemplateColumns += " 100px"   
     let cellElements = gridChildNodes.length
-    
-    
+
+    // In the case of no nodes, the for loop would not run.
+    // Therefore, we add one cell since there are no rows or columns
+    if (cellElements - 1 === -1) {
+        cell = document.createElement("div")
+        cell.setAttribute("class", "box")
+        gridBoardEl.appendChild(cell)
+    }
+
     // Add an element after the last element of a row
     // We know where this is based on the number of columns
     for (let i = cellElements - 1; i >= 0; i -= cellsToSkip) {
@@ -32,8 +39,35 @@ const addCol = () => {
         gridChildNodes[i].after(cell)
     }
 
+
 }
 
 addColBtn.addEventListener("click", addCol)
 
 ////////////////////////////////////////////////////////////////////////
+
+
+// The below code is used to add a column to a filled grid
+
+const removeCol = () => {
+    
+    //Find the number of columns so we know where to remove new cells 
+    tempString = gridBoardEl.style.gridTemplateColumns.split(" ")
+    let cellsToSkip = tempString.length // number to decrement for loop by
+
+    // Removing a value deletes a column recognized by CSS Grid.
+    let index = gridBoardEl.style.gridTemplateColumns.lastIndexOf("100px")
+    gridBoardEl.style.gridTemplateColumns = gridBoardEl.style.gridTemplateColumns.slice(0, index)
+    let cellElements = gridChildNodes.length
+    
+    
+    // Remove the last element of the row
+    // We know where this is based on the number of columns
+    for (let i = cellElements - 1; i >= 0; i -= cellsToSkip) {
+        gridChildNodes[i].remove()
+    }
+
+}
+
+
+removeColBtn.addEventListener("click", removeCol)
